1f9e662879109f6f1f9e6ba62f131713efc67677
themylogin <themylogin@gmail.com>
2021-09-28 23:42:51 +0200

    Update rclone to the latest version

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -ex
-VERSION=v1.53.1
+VERSION=v1.56.1
 wget https://downloads.rclone.org/$VERSION/rclone-$VERSION-linux-amd64.zip
 unzip rclone-$VERSION-linux-amd64.zip
 mv rclone-$VERSION-linux-amd64/rclone .
